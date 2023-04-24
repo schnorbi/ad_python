9766aa06df62c9aa347b60065e1ba419a4a4c72c
Corbian Reiser <corb.reiser@gmail.com>
2023-04-24 11:20:45 +0200

    Prority Queue.py

---
+++ Priority Queue.py
@@ -0,0 +1,32 @@
+class PQueue():
+    def __init__(self):
+        self.__data = []
+
+    def insert(self, v):
+        self.__data.append(v)
+        self.__data.sort(reverse=True)
+
+    def remove_largest(self):
+        return self.__data.pop(0)
+
+    def largest(self):
+        return self.__data[0]
+
+    def is_empty(self):
+        return not (bool(self.__data))
+
+#Testcases
+q = PQueue()
+q.insert(23)
+q.insert(42)
+q.insert(-3)
+e = q.remove_largest()
+print(e)
+
+q = PQueue()
+q.insert(42)
+_ = q.remove_largest()
+print(q.is_empty())
+
+q = PQueue()
+print(q.is_empty())
